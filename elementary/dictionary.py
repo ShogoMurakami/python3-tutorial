35cc353949c7108ec0639f901be6e55718ce8bb6
ShogoMurakami <shogopc1@gmail.com>
2019-11-16 18:17:56 +0900

    update

---
+++ elementary/dictionary.py
@@ -0,0 +1,8 @@
+# coding: utf-8
+
+team7 = {"naruto": 1000, "sasuke": 900, "sakura": 800, "kakashi": 700}
+
+for ninja, power in team7.items():
+    print(ninja + ": " + str(power))
+
+    
---
+++ elementary/tutorial_func.py
@@ -0,0 +1,7 @@
+# coding: utf-8
+
+def battle (*enemies):
+    for enemy in enemies:
+        print(enemy)
+
+battle("大蛇丸", "ペイン", "六道仙人", "木の葉丸")
