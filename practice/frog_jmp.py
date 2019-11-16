8e50a04bf82daa29653f786fd1eeceb08079a4ca
ShogoMurakami <shogopc1@gmail.com>
2019-11-16 23:20:42 +0900

    add frog

---
+++ practice/frog_jmp.py
@@ -0,0 +1,19 @@
+# coding: utf-8
+
+def solution(X, Y, D):
+
+    times = 0
+
+    while X < Y:
+        X += D
+        times += 1
+
+    else:
+        return times
+
+
+X = 10
+Y = 85
+D = 30
+
+print(solution(X, Y, D))
