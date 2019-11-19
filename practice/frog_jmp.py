158198ac5b41c9ee11e2cf5126b9c727a7599d8f
ShogoMurakami <shogopc1@gmail.com>
2019-11-19 17:55:12 +0900

    Update

--- practice/frog_jmp.py
+++ practice/frog_jmp.py
@@ -1,4 +1,4 @@
-# coding: utf-8
+# coding: utf-8k
 
 def solution(X, Y, D):
 
---
+++ practice/test_scoring.py
@@ -0,0 +1,16 @@
+# coding: utf-8
+# Your code here!
+
+n = input().split()
+
+s =[input().split() for i in range(int(n[0]))]
+
+for (number, j) in enumerate(s):
+    result = int(j[0]) - (int(j[1]) * 5 )
+
+    if result < 0:
+        result = 0
+
+    if result >= int(n[1]):
+        print(number + 1)
+    
