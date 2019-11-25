26e5c840aabccefb6b04f3d971e26bcdbaa19ee7
ShogoMurakami <shogopc1@gmail.com>
2019-11-25 21:36:44 +0900

    add perm check

---
+++ practice/perm_check.py
@@ -0,0 +1,20 @@
+# coding: utf-8
+
+# PermCheck in Codility (Score: 66)
+# 順列なら1を返し、そうでなければ0を返す
+
+def solution(A):
+
+    i = 1
+    end = len(A)
+
+    while i <= end:
+        if A.count(i) == 0:
+            return 0
+        i +=1
+    else:
+        return 1
+
+A = [4,1,3,2,5]
+
+print(solution(A))
